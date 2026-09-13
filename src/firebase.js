c986e54c39c3d0d048a40b12c54f0a952e3fb7ae
mtt045519-dot <mtt045519@gmail.com>
2026-09-13 21:03:07 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -16,5 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const auth = getAuth(app);
 export const messaging = getMessaging(app);
