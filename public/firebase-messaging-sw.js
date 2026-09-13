2a1138d21f7c452f4d247ecb00dcda92ce65f00d
mtt045519-dot <mtt045519@gmail.com>
2026-09-13 20:55:43 +0600

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,22 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyD3_UWOLDDYfjQjeWCBY3q7Ou-lwkmOGdo",
+  authDomain: "urfftour.firebaseapp.com",
+  projectId: "urfftour",
+  storageBucket: "urfftour.firebasestorage.app",
+  messagingSenderId: "809102834134",
+  appId: "1:809102834134:web:6a08691b5cc23510c5cc77"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon-192.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
